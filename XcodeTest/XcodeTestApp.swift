f053ce00cb02435f41a3ff83330fc796d404e05b
a$ong <fwscool@163.com>
2024-11-15 01:13:07 +0800

    Initial Commit

---
+++ XcodeTest/XcodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTestApp.swift
+//  XcodeTest
+//
+//  Created by 樊万松 on 2024/11/15.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeTestTests/XcodeTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  XcodeTestTests.swift
+//  XcodeTestTests
+//
+//  Created by 樊万松 on 2024/11/15.
+//
+
+import Testing
+
+struct XcodeTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
